a4f4650c7aba28e883efb3c8be453d09226bdb2a
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-03-14 11:33:29 +0200

    end day 13march2023

--- src/partials/header.js
+++ src/partials/header.js
@@ -3,21 +3,27 @@ import Notiflix from 'notiflix';
 
 //declaration
 let searchSection = document.querySelector(".search-section");
+let form = document.querySelector(".searchbar");
 console.log(searchSection);
-let inputText= "";
+let inputText;
 let citiesArr = [];
+let retrievedArray = [];
 let cityInput = document.querySelector("#city-input");
 let saveCitySVG = document.querySelector("#star");
 let savedCities = document.querySelector(".saved-cities");
 let savedCityy = document.querySelector(".saved-city");
 let closeCity = document.querySelector(".close-city");
+let localStoreCities = "localCities";
+
+
+
 
 
 //functions
 getInput = event => {inputText = event.currentTarget.value.toLowerCase(); return inputText;}
 
 savedCityMarkup = (event) =>
-{ if(!inputText.length == 0){
+{ if(!inputText.length == 0 && !retrievedArray.includes(inputText)){
   if(!citiesArr.includes(inputText)){let markup = `<li class="saved-city" id="${inputText}">
                                                      <span class="city">${inputText}</span>
                                                      <svg class="close-city">
@@ -26,7 +32,8 @@ savedCityMarkup = (event) =>
                                                     </li>`;
                                       savedCities.insertAdjacentHTML("beforeend", markup);
                                       citiesArr.push(inputText.toLowerCase());
-                                      console.log("For add:",citiesArr);
+                                      console.log("Add array:",citiesArr);
+                                      localStorage.setItem(localStoreCities, JSON.stringify(citiesArr));
                                       return citiesArr;}
   else{Notiflix.Notify.warning('City already as favorite.')}}
   else(Notiflix.Notify.warning('Enter a valid city name.'))
@@ -34,8 +41,6 @@ savedCityMarkup = (event) =>
 
 emptyMarkup = (event) => {let g = event.target.parentNode;
                           let gg = g.parentNode;
-                          console.log(g.nodeName);
-                          console.log(g.parentNode.nodeName);
                           let removeing;
                           if(g.nodeName == "svg"){removeing = document.querySelector(`#${gg.getAttribute("id")}`);
                                                   removeing.remove();}
@@ -43,12 +48,27 @@ emptyMarkup = (event) => {let g = event.target.parentNode;
                           removeing.remove();}
                           let idx = citiesArr.indexOf(removeing);
                           citiesArr.splice(idx,1);
-                          console.log(citiesArr)
+                          localStorage.setItem(localStoreCities, JSON.stringify(citiesArr));
+                          console.log("Empty array", citiesArr);
                              }
-                           
+
+function storedCities(){let retrievedArrayAsString = localStorage.getItem(localStoreCities);
+                        retrievedArray = JSON.parse(retrievedArrayAsString);
+                        console.log("Local saved: ", retrievedArray);
+                        retrievedArray.forEach(city => {let markupSaved = `<li class="saved-city" id="${city}">
+                                                          <span class="city">${city}</span>
+                                                          <svg class="close-city">
+                                                            <use href = "/header-symbol-defs.5e7c9225.svg#icon-cancel-circle"></use>
+                                                          </svg>
+                                                        </li>`;
+                        savedCities.insertAdjacentHTML("beforeend", markupSaved)});
+                        return retrievedArray;
+                        }
+storedCities()
+
 //eventlistener
 cityInput.addEventListener("input", getInput);
 saveCitySVG.addEventListener("click", savedCityMarkup);
 savedCities.addEventListener("click", emptyMarkup);
 
-//export {searchSection, inputText, citiesArr, cityInput, saveCitySVG, savedCities, savedCityy, closeCity, getInput, savedCityMarkup, emptyMarkup};
+
