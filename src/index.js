02a02ecec741c560cab1f3640848984f3fa683ef
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-03-17 19:04:13 +0200

    fetch without translate

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+
+
 import "./partials/header";
+
 
 
 
--- src/partials/header.js
+++ src/partials/header.js
@@ -2,36 +2,25 @@ import { remove } from 'lodash';
 import Notiflix from 'notiflix'; 
 
 //declaration
+let body = document.querySelector("body");
 let searchSection = document.querySelector(".search-section");
 let form = document.querySelector(".searchbar");
-console.log(searchSection);
-let inputText;
+let inputText = ``;
 let retrievedArray = [];
 let citiesArr = [];
 let cityInput = document.querySelector("#city-input");
 let saveCitySVG = document.querySelector("#star");
+let searchCitySVG = document.querySelector("#gps");
 let savedCities = document.querySelector(".saved-cities");
-let savedCitiesWidth = savedCities.parentNode.offsetWidth;
-console.log(savedCities);
-//console.log("x",savedCities.attributes.length);
-let savedCityy = document.querySelector(".saved-city");
-let closeCity = document.querySelector(".close-city");
+let savedCitiesWidth = savedCities.parentNode.offsetWidth; 
 let localStoreCities = "localCities";
 let prevButton = document.querySelector(".prevButton");
 let nextButton = document.querySelector(".nextButton");
-let nrOfCities;
-let currentIndex = 0;
-let itemWidth = 105;
 let itemsPerPage;
 if (savedCitiesWidth < 220){itemsPerPage = 2;}
 else{itemsPerPage = 4;}
-let move = 110;
-let count = 0;
-let calcTrans;
-let scrolled;
-let maxCount = citiesArr.length;
 let initial = 0;
-let moveTrans = 110;
+
 //functions
 
 //scroll saved city
@@ -50,7 +39,6 @@ savedCityMarkup = (event) =>
 {let retrievedArrayAsString = localStorage.getItem(localStoreCities);
   retrievedArray = JSON.parse(retrievedArrayAsString);
   citiesArr = [...retrievedArray];
-  console.log(citiesArr);
   if(inputText == undefined){Notiflix.Notify.warning('Enter a valid city name.')}
   else if(inputText.length !== 0)
       {if(!citiesArr.includes(inputText)){ citiesArr.push(inputText.toLowerCase());
@@ -58,7 +46,6 @@ savedCityMarkup = (event) =>
                                           item.classList.add("saved-city");
                                           item.setAttribute("id", `${inputText}`);
                                           item.setAttribute("title", `${inputText}`);
-                                          console.log("length ", citiesArr.length);
                                           let markup = `<span class="city">${inputText}</span>
                                                      <svg class="close-city" title = ${inputText}>
                                                       <use href = "/header-symbol-defs.5e7c9225.svg#icon-cancel-circle"></use>
@@ -66,26 +53,23 @@ savedCityMarkup = (event) =>
                                       savedCities.append(item);
                                       item.insertAdjacentHTML("beforeend", markup);
                                       localStorage.setItem(localStoreCities, JSON.stringify(citiesArr));
-                                      console.log("Add array:",citiesArr);
                                       btnShow();
+                                      form.reset();
                                       return citiesArr;}
        else{Notiflix.Notify.warning('City already as favorite.')}}
   else(Notiflix.Notify.warning('Enter a valid city name.'));
 
 };
 
-emptyMarkup = (event) => {let g = event.target.parentNode;
-                          console.log("g 1",g);
+emptyMarkup = (event) => {let g = event.target;
                           let gg = g.parentNode;
                           let removeing;
-                          if(g.nodeName == "svg"){gg.remove(); console.log("gg 1",gg);
-                          citiesArr.splice(citiesArr.indexOf(gg.getAttribute("title")),1);}
-                          else{console.log("g 2",g); g.remove();
-                                citiesArr.splice(citiesArr.indexOf(g.getAttribute("title")),1);}
-                          
+                          if(g.nodeName == "span"){console.log("gg 0: all good"); return;}
+                          else if(g.nodeName == "svg"){gg.remove();
+                                                  citiesArr.splice(citiesArr.indexOf(gg.getAttribute("title")),1);}
+                          else if(g.nodeName == "use"){gg.parentNode.remove();
+                                citiesArr.splice(citiesArr.indexOf(gg.parentNode.getAttribute("title")),1);}
                           localStorage.setItem(localStoreCities, JSON.stringify(citiesArr));
-                          console.log(searchSection);
-                          console.log("Empty array", citiesArr);
                           initial = 0;
                           savedCities.style.transform = `translatex(0px)`;
                           prevButton.style.visibility = "hidden";
@@ -95,9 +79,7 @@ emptyMarkup = (event) => {let g = event.target.parentNode;
 function storedCities()
 {let retrievedArrayAsString = localStorage.getItem(localStoreCities);
 retrievedArray = JSON.parse(retrievedArrayAsString);
-console.log("Local saved: ", retrievedArray);
 citiesArr = [...retrievedArray];
-console.log(citiesArr)
 if(citiesArr.length !== 0){citiesArr.forEach(city => {let item = document.createElement("li");
                                                       item.classList.add("saved-city");
                                                       item.setAttribute("id", `${city}`);
@@ -108,37 +90,61 @@ if(citiesArr.length !== 0){citiesArr.forEach(city => {let item = document.create
                                                                                             </svg>`;
                                                       savedCities.append(item);
                                                       item.insertAdjacentHTML("beforeend", markupSaved)});
-                                                      btnShow();
-                                                      return retrievedArray;}
+                                                      btnShow();                                                      
+                                                      return retrievedArray;}                                              
 }
 storedCities()
 
 
-function showNextItems(){console.log("initial=0 ", initial);                     
-                         let maxTrans = (citiesArr.length - itemsPerPage) * 110;                   
+function showNextItems(){let maxTrans = (citiesArr.length - itemsPerPage) * 110;                   
                          if(initial < maxTrans){initial += 110;
-                                                 savedCities.style.transform = `translatex(-${initial}px)`;
-                                                 console.log("NEXT initial + maxTrans: ", initial, maxTrans);};
+                                                 savedCities.style.transform = `translatex(-${initial}px)`};
                          if(initial == maxTrans){nextButton.style.visibility = "hidden"};
                                                  prevButton.style.visibility = "visible";
                                                   return initial;}
-function showPreviousItems(){ console.log("PREV initial 1", initial);
-                              if(initial > 0){initial -= 110;
+function showPreviousItems(){if(initial > 0){initial -= 110;
                                                 savedCities.style.transform = `translatex(-${initial}px)`;
-                                                console.log("PREV initial  2 ", initial);
                                                 if (initial == 0){nextButton.style.visibility = "visible";
-                                                                  prevButton.style.visibility = "hidden";
-                                                                  console.log("PREV initial 3 ", initial);}}
-                                                return initial;}
+                                                                  prevButton.style.visibility = "hidden";}}
+                                                return initial;}                                       
 
-                                     
+function inputSearch(event){
+                       console.log("inputText submit enter",inputText);
+                       let apiUrl = `https://pixabay.com/api/?key=42799638-b50871d8c9a958480a9d6ba7c&&safesearch=true&image_type=photo&pretty=true&q=${inputText}`;
+                       console.log(apiUrl)   
+                       fetch(apiUrl).then(response => {if (!response.ok) {console.log("response No:", body);}
+                                                        else{console.log("response YES:", body)
+                                                              return response.json()};})
+                                    .then(data => {for(let hit of data.hits){if (hit.tags.includes("architecture") || hit.tags.includes("building") || hit.tags.includes("city") || hit.tags.includes("landscape") || hit.tags.includes("landmark")){largeImageARR.push(hit.largeImageURL);};};
+                                                                             if(largeImageARR.length > 1){body.style.backgroundImage = `url('${largeImageARR[1]}')`;}
+                                                                             else{body.style.backgroundImage = `url('${largeImageARR[0]}')`}});
+                       largeImageARR=["https://e0.pxfuel.com/wallpapers/685/451/desktop-wallpaper-summer-day-sky-midday-sky.jpg"];
+                       console.log("inputText submit exit",inputText);
+                       form.reset();                                                      
+                          }
 
+function addInputText(event){console.log("inputText add enter",inputText);
+                            let www = event.target;
+                             inputText = www.textContent;
+                             console.log("inputText add set",inputText);
+                             inputSearch();
+                             largeImageARR=["https://e0.pxfuel.com/wallpapers/685/451/desktop-wallpaper-summer-day-sky-midday-sky.jpg"];
+                             console.log("inputText add exit",inputText);
+                             inputText = ``;
+                             }                                   
+
+function inputSearchDefault(event){event.preventDefault();
+                                   inputSearch();}         
 //eventlistener
 cityInput.addEventListener("input", getInput);
 saveCitySVG.addEventListener("click", savedCityMarkup);
 savedCities.addEventListener("click", emptyMarkup);
 nextButton.addEventListener("click", showNextItems);
 prevButton.addEventListener("click", showPreviousItems);
+form.addEventListener("submit", inputSearchDefault);
+searchCitySVG.addEventListener("click", inputSearchDefault);
+savedCities.addEventListener("click", addInputText);
+
 
 /*
 let body = document.querySelector("body")
@@ -176,5 +182,4 @@ romanian_city_name = "București"
 english_city_name = translate_city_name(romanian_city_name)
 print(f"Romanian: {romanian_city_name}, English: {english_city_name}")
 */
-
 
